bed527a93f27a29ea93b44983876ab0a9bb140c6
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2025-09-11 14:59:19 -0300

    wip: cli

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from tui import get_tui, cprint
+from downloader import download
 from colors import *
 
+class DownloaderLogger:
+    def debug(self, msg):
+        # yt-dlp sends a lot of "debug" messages here, including ffmpeg commands
+        # if msg.startswith("[debug]"):
+        #     return   # ignore debug spam
+        cprint(f"[DEBUG] {msg}")
+
+    def info(self, msg):
+        cprint(f"{BLUE}[INFO] {msg}{EBLUE}")
+
+    def warning(self, msg):
+        cprint(f"{YELLOW}[WARNING] {msg}{EYELLOW}")
+
+    def error(self, msg):
+        cprint(f"{RED}[ERROR] {msg}{ERED}")
+
+
 def handler(text: str):
-    cprint(f"{BLUE}You entered:{EBLUE}")
-    cprint(f"{RED}{'-'*40}{ERED}")
+    # cprint(f"{BLUE}You entered:{EBLUE}")
     cprint(f"{BOLD}{text}{EBOLD}")
-    cprint(f"{RED}{'-'*40}{ERED}")
+    download(text, "audio", DownloaderLogger())
+    
+    # cprint(f"{RED}{'-'*40}{ERED}")
+    # cprint(f"{RED}{'-'*40}{ERED}")
 
 def main():
     get_tui(handler).run()
